9f87a70a21a9b1b42090343465b5efbcdda2a505
anasAbounouar <anas.2002.abounouar@gmail.com>
2023-09-20 00:40:56 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-createApp(App).use(store).use(router).mount("#app");
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/main.css";
+import "./scss/all.min.css";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import "./scss/framework.css";
+import "normalize.css";
+const app = createApp(App);
+app.mixin({
+  created() {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: true,
+      // More AOS options...
+    });
+  },
+});
+app.use(router);
+app.use(store);
+app.mount("#app");
