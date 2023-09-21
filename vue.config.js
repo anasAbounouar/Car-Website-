7716635dcf176b5d4c732a3ba6b0da48db35e7b6
anasAbounouar <anas.2002.abounouar@gmail.com>
2023-09-21 19:45:18 +0100

    deploying

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/sam/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-pages/" : "/",
 });
